29247c64bab75ff98082eb3f591f7c8aba50f6e5
shaunpetro <shaun.p.mckoy@gmail.com>
2023-12-15 10:01:54 +0000

    adist.c: updates

--- adist.c
+++ adist.c
@@ -1,14 +1,28 @@
 #include "shellhead.h"
 
 /**
- * _mhist  - shows te history by list from 0,
- *           one cmonnand per line
+ * _mhist - displays the history by list
  * @info: struct with possible arg's
  *
- * Return: 0 always
+ * Return: 0
  */
 
 int _mhist(info_t *info)
+{
+	print_list(info->history);
+
+	return (0);
+}
+
+/**
+ * alias_us - sets alias to str
+ * @info: parameter struct
+ * @str: string
+ *
+ * Return: 0 success, 1 error
+ */
+
+int alias_us(info *info, char *str)
 {
 	char *p, c;
 	int ret;
@@ -18,15 +32,16 @@ int _mhist(info_t *info)
 		return (1);
 	c = *p;
 	*p = 0;
-	ret = delnode_at_index(&(info->alias, node_swith(info->alias, str, -1)));
+	ret = delnode_at_index(&(info->alias),
+			get_node_index(info->alias, node_swith(info->alias, str, -1)));
 	*p = c;
 	return (ret);
 }
 
 /**
  * salias - sets alias to str
- * @info: struct parameter with possible arg's
- * @str: alias str
+ * @info: parameter struct
+ * @str: string
  *
  * Return: 0 success, 1 error
  */
@@ -40,15 +55,17 @@ int salias(info_t *info, char *str)
 		return (1);
 	if (!*++p)
 		return (alias_us(info, str));
+
 	alias_us(info, str);
+
 	return (add_end_node(&(info->alias), str, 0) == NULL);
 }
 
 /**
- * print_alias - prints the alias str
+ * print_alias - prints alias
  * @node: node
  *
- * Return: 0 success or 1 error
+ * Return: 0 success 1 error
  */
 
 int print_alias(list_t *node)
@@ -58,19 +75,22 @@ int print_alias(list_t *node)
 	if (node)
 	{
 		p = _fcharstr(node->str, '=');
+
 		for (a = node->str; a <= p; a++)
+		{
 			_putchar(*a);
-		_putchar('\'');
-		_puts(p + 1);
-		_puts("'\n");
+			_putchar('\'');
+			_puts(p + 1);
+			_puts('"\n");
+		}
 		return (0);
 	}
 	return (1);
 }
 
 /**
- * _myalias - imatates te built in alias
- * @info: struct parameter with possible arg's
+ * _myalias - initiates inbuiltr alias
+ * @info: parameter struct
  *
  * Return: 0 always
  */
